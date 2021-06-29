55b4cbd16fecc2062c4ce6f559eff723bea85d33
jnz31 <jnz31@users.noreply.github.com>
2021-06-29 20:24:41 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,11 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { createStore } from 'vuex'
 import getters from './getters'
 import mutations from './mutations'
 import actions from './actions'
 
-Vue.use(Vuex)
-
 const { state } = __VUE_WORDPRESS__
 
-export default new Vuex.Store({
+export default createStore({
   state,
   getters,
   mutations,
