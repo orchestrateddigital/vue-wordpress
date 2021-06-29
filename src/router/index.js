f849a3fd95eb1280b5a813e723771fa62e9fad4c
jnz31 <jnz31@users.noreply.github.com>
2021-06-29 20:25:07 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 
 const { url } = __VUE_WORDPRESS__.routing
@@ -7,10 +6,8 @@ const { url } = __VUE_WORDPRESS__.routing
 // scroll position is handled in @after-leave transition hook
 if ('scrollRestoration' in window.history) window.history.scrollRestoration = 'manual'
 
-Vue.use(Router)
-
-export default new Router({
-  base: url.replace(window.location.origin, ''),
-  mode: 'history',
+export default createRouter({
+  // base: url.replace(window.location.origin, ''),
+  history: createWebHistory(url.replace(window.location.origin, '')),
   routes: routes
 })
